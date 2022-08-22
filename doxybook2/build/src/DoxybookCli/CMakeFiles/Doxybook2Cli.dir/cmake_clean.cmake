0760dbbadc536163ad34cc20cd793c34e72e3b8f
Ross Clark <r.clark1242@gmail.com>
2022-08-22 17:39:40 +0100

    Edited doxybook2 source code

---
+++ doxybook2/build/src/DoxybookCli/CMakeFiles/Doxybook2Cli.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Doxybook2Cli.dir/main.cpp.o"
+  "CMakeFiles/Doxybook2Cli.dir/main.cpp.o.d"
+  "doxybook2"
+  "doxybook2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Doxybook2Cli.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
