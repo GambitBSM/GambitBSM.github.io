6ee208cb178aed3a1a3c3c4d5e8eb19468950a85
Are Raklev <ahye@uio.no>
2025-06-24 17:19:06 +0100

    Added markdown file for ColliderBit analysis

--- scripts/generate_analysis_list.py
+++ scripts/generate_analysis_list.py
@@ -1,3 +1,39 @@
+"""
+
+This takes the JSON file analyses.json generated in a GAMBIT CI job that contains 
+information on the analysis included in ColliderBit and uses this to generate a 
+markdown page listing all the analysis.
+
+TODO: Where to put the analyses.json file
+
+"""
+
 import json
 
 print("Generating list of ColliderBit analysis...")
+
+# Opening JSON file and returning dictionary
+json_file = open('analyses.json',)
+data = json.load(json_file)
+
+# Find all analysis present by inspire id
+for analysis in data['analyses']:
+  print(analysis['inspire_id'])
+
+
+##################
+# Make markdown
+##################
+markdown = f"""
+## List of ColliderBit analysis in GAMBIT {data['version']}
+
+"""
+
+for analysis in data['analyses']:
+    markdown += f"{analysis["implementations"]["name"]}\n"
+print(markdown)
+
+
+# Write markdown file
+markdown_file = open("../content/en/documentation/physics/analyses.md", 'w')
+markdown_file.write(markdown)
